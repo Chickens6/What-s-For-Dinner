6b683e72ff19fd2ec7fda238e2610a174cb14b96
KarXxn <karenlozano401@gmail.com>
2019-07-19 09:28:24 -0500

    color
    and fix

--- app/src/main/java/com/example/newnewnewproject/MainActivity.java
+++ app/src/main/java/com/example/newnewnewproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.newnewnewproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,7 +12,9 @@ View view ;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         view= this.getWindow().getDecorView();
                 view.setBackgroundResource(R.color.black);
     }
 }
+
