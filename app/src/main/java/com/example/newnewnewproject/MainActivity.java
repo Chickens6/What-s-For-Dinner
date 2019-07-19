26bdd909ffd2740ea7dbabe4e1439b3410307a80
KarXxn <karenlozano401@gmail.com>
2019-07-18 22:35:13 -0500

    backgrounf
    black

--- app/src/main/java/com/example/newnewnewproject/MainActivity.java
+++ app/src/main/java/com/example/newnewnewproject/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.newnewnewproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+View view ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view= this.getWindow().getDecorView();
+                view.setBackgroundResource(R.color.black);
     }
 }
