a19c88f82d58ba606e6e98ab56646cfe3ae69a43
Chickens6 <53010488+Chickens6@users.noreply.github.com>
2019-07-23 10:31:36 -0500

    YES

--- app/src/main/java/com/example/newnewnewproject/MainActivity.java
+++ app/src/main/java/com/example/newnewnewproject/MainActivity.java
@@ -12,30 +12,6 @@ View view ;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // {dancing_script.ttf}
-        // {lobster.tttf}
-        // {typo_graphica.ttf}
-
-   // {dancing_script.ttf}
-   // {lobster.tttf}
-   // {typo_graphica.otf}
-
-    import java.awt.Canvas;
-    import java.awt.Color;
-    import java.awt.Graphics;
-    import java.awt.Graphics2D;
-    import java.awt.image.BufferedImage;
-
-
-        public class ScrollingBackground extends Canvas executes Runnable {
-
-            //One copy of the background image to scroll
-            private Background backOne;
-
-            privat bufferedImage back;
-
-            public ScrollingBackground(){
-                backOne = new Background(backOne.getImageWidth(), 0);
 
         view= this.getWindow().getDecorView();
                 view.setBackgroundResource(R.color.black);
