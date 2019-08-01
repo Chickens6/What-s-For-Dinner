2d761106418ce3bdd20547f04e68a610c981d9e2
dannygarcia4601996 <53010601+dannygarcia4601996@users.noreply.github.com>
2019-08-01 10:00:25 -0500

    Update Main2Activity.java

--- app/src/main/java/com/example/newnewnewproject/Main2Activity.java
+++ app/src/main/java/com/example/newnewnewproject/Main2Activity.java
@@ -3,12 +3,29 @@ package com.example.newnewnewproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button AMERICAN = findViewById(R.id.button1);
+        AMERICAN.setOnClickListener(this);
     }
-}
+
+    public void onClick(View view){
+        int select = view.getId();
+
+        if(select == R.id.button1);
+
+        }
+
+    }
+
+
+
+
