7fc1a714e828b5d409b283ec157e499ee8818d5d
Jasmin Aquines <jasminaquines@gmail.com>
2019-08-01 16:53:46 -0500

    Update MainActivity.java
    Fixed

--- app/src/main/java/com/example/newnewnewproject/MainActivity.java
+++ app/src/main/java/com/example/newnewnewproject/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     View view;
-    float x1,x2,y1,y2;
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,39 +21,32 @@ public class MainActivity extends AppCompatActivity {
         view.setBackgroundResource(R.color.black);
 
 
-}
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         //return super.onTouchEvent(event);
 
         //public boolean onTouchEvent (MotionEvent touchEvent){
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    x1 = event.getX();
-                    y1 = event.getY();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    x2 = event.getX();
-                    y2 = event.getY();
-                    if (x1 < x2) {
-                         Intent i = new Intent(MainActivity.this, SwipeLeft.class);
-                        startActivity(i);
-                    } else if (x1 > x2) {
-                        Intent i = new Intent(MainActivity.this, SwipeRight.class);
-                       startActivity(i);
-                    }
-                    break;
-<<<<<<< HEAD
-            }return false;
-    }
-=======
-            } return false;
-         
-    }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                if (x1 < x2) {
+                    Intent i = new Intent(MainActivity.this, SwipeLeft.class);
+                    startActivity(i);
+                } else if (x1 > x2) {
+                    Intent i = new Intent(MainActivity.this, SwipeRight.class);
+                    startActivity(i);
+                }
+                break;
 
-
->>>>>>> e99ccbb751c8fa2495d41111d96df208fed463b9
+        }
+        return false;
+    }
 }
-
 
