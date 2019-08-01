ea1b1915ccd353234692fcc98a535601383d295a
dannygarcia4601996 <53010601+dannygarcia4601996@users.noreply.github.com>
2019-08-01 10:36:35 -0500

    Update Main2Activity.java

--- app/src/main/java/com/example/newnewnewproject/Main2Activity.java
+++ app/src/main/java/com/example/newnewnewproject/Main2Activity.java
@@ -15,16 +15,37 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
         Button AMERICAN = findViewById(R.id.button1);
         AMERICAN.setOnClickListener(this);
+
+        Button MEXICAN = findViewById(R.id.button2);
+        MEXICAN.setOnClickListener(this);
+
+        Button ITALIAN = findViewById(R.id.button3);
+        ITALIAN.setOnClickListener(this);
+
+        Button FRENCH = findViewById(R.id.button4);
+        FRENCH.setOnClickListener(this);
+
+        Button CHINESE = findViewById(R.id.button5);
+        CHINESE.setOnClickListener(this);
+
+        Button GREEK = findViewById(R.id.button6);
+        GREEK.setOnClickListener(this);
+
     }
 
     public void onClick(View view){
         int select = view.getId();
 
         if(select == R.id.button1);
-
-        }
-
+        if(select == R.id.button2);
+        if(select == R.id.button3);
+        if(select == R.id.button4);
+        if(select == R.id.button5);
+        if(select == R.id.button6);
     }
+
+
+}
 
 
 
