b09821ff105316f65a015ffc527a5acca30213cf
silviamarianadlphdz16 <53010548+silviamarianadlphdz16@users.noreply.github.com>
2019-08-02 09:27:37 -0500

    update food.java

--- app/src/main/java/com/example/newnewnewproject/Food.java
+++ app/src/main/java/com/example/newnewnewproject/Food.java
@@ -2,7 +2,7 @@ package com.example.newnewnewproject;
 
 public class Food {
     //to get food -> Food.french[number-1] = {"name","url"}
-    //Food.frech[number-1][0]
+    //Food.french[number-1][0]
     public static String[][] french = {{"Ratatouille Recipe", "https://www.thespruceeats.com/french-vegetarian-recipes-1375858"},
             {"Cauliflower Gratin Recipe ", "//www.thespruceeats.com/french-vegetarian-recipes-1375858"},
             {"Tempeh and Mushroom Bourguignon Recipe  ", "https://www.onegreenplanet.org/vegan-recipe/tempeh-and-mushroom-bourguignon/"},
@@ -14,7 +14,25 @@ public class Food {
             {"A tres vegan cassoulet ","https://holycowvegan.net/vegan-cassoulet"},
             {"Socca ( Farinata) - Chickpea Flour","https://simple-veganista.com/socca-farinata/"},
             {"Veggie Pan Bagnat( White Bean Nicoise Salad Sandwich ","https://simple-veganista.com/pan-bagnat-white-bean-nicoise-salad/"},
-            {"Medterranean Tomato Tart ","https://veganuary.com/recipes/mediterranean-tomato-tart/"},
+            {"Medterranean Tomato Tart ","https://veganuary.com/recipes/mediterranean-tomato-tart/"}};
+
+
+    public static String[][] mexican = {{"kale chip nachos", "https://minimalistbaker.com/kale-chip-nachos-30-minutes/"},
+            {"ceviche","https://www.veganmexicanfood.com/recipes/ceviche"},
+            {"vegan tacos","https://veganheaven.org/recipe/vegan-tacos-with-lentil-walnut-meat/"},
+            {"vegan chiles en nogada ","https://dorastable.com/vegan-chiles-en-nogada/"},
+            {"mushroom quesadilla","https://www.veganricha.com/2019/07/vegan-mushroom-quesadilla.html"},
+            {"mexican bean burgers","https://www.allrecipes.com/recipe/13963/mexican-bean-burgers/"},
+            {"chipotle chili with avocado","https://cookieandkate.com/butternut-squash-chipotle-chili-with-avocado/"},
+            {"chili stuffed quesadillas","https://www.vegetariantimes.com/recipes/chili-stuffed-enchiladas-with-roasted-mushroom-sauce"},
+            {"lazy lentils","https://veganuary.com/recipes/lazy-lentils/"},
+            {"vegeterian mushroom quesadillas","https://www.thespruceeats.com/vegetarian-mushroom-fajitas-recipe-3377114"},
+            {"cauliflower tacos","https://minimalistbaker.com/roasted-cauliflower-tacos-with-chipotle-romesco/"},
+            {"mexican potatoes","https://www.veganmexicanfood.com/recipes/mexican-potatoes"}};
+
+
+
+
 
 
 }
