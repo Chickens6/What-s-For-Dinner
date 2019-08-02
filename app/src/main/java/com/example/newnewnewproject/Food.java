4bfc327eb875f30228cfe0880986a77131c267d2
silviamarianadlphdz16 <53010548+silviamarianadlphdz16@users.noreply.github.com>
2019-08-02 09:44:15 -0500

    update

--- app/src/main/java/com/example/newnewnewproject/Food.java
+++ app/src/main/java/com/example/newnewnewproject/Food.java
@@ -8,7 +8,6 @@ public class Food {
             {"Tempeh and Mushroom Bourguignon Recipe  ", "https://www.onegreenplanet.org/vegan-recipe/tempeh-and-mushroom-bourguignon/"},
             {"Potato Leek Quiche", "ttps://www.onegreenplanet.org/vegan-recipe/potato-leek-quiche/"},
             {"Sun - Dried Tomato and Zucchini Tarts", "https://www.onegreenplanet.org/vegan-recipe/sun-dried-tomato-and-zucchini-tarts/"},
-<<<<<<< HEAD
             {"Tomatoes Provencal ","https://www.onegreenplanet.org/vegan-recipe/tomatoes-provencal/"},
             {"Vegan Leek Quiche ","https://holycowvegan.net/vegan-leek-quiche/"},
             {"Vegan Savory Herb French Toast ","https://holycowvegan.net/vegan-savory-herb-french-toast/"},
@@ -30,20 +29,5 @@ public class Food {
             {"vegeterian mushroom quesadillas","https://www.thespruceeats.com/vegetarian-mushroom-fajitas-recipe-3377114"},
             {"cauliflower tacos","https://minimalistbaker.com/roasted-cauliflower-tacos-with-chipotle-romesco/"},
             {"mexican potatoes","https://www.veganmexicanfood.com/recipes/mexican-potatoes"}};
-
-
-
-
-
-=======
-            {"Tomatoes Provencal ", "https://www.onegreenplanet.org/vegan-recipe/tomatoes-provencal/"},
-            {"Vegan Leek Quiche ", "https://holycowvegan.net/vegan-leek-quiche/"},
-            {"Vegan Savory Herb French Toast ", "https://holycowvegan.net/vegan-savory-herb-french-toast/"},
-            {"A tres vegan cassoulet ", "https://holycowvegan.net/vegan-cassoulet"},
-            {"Socca ( Farinata ) - Chickpea Flour", "https://simple-veganista.com/socca-farinata/"},
-            {"Veggie Pan Bagnat( White Bean Nicoise Salad Sandwich ", "https://simple-veganista.com/pan-bagnat-white-bean-nicoise-salad/"},
-            {"Mediterranean Tomato Tart ", "https://veganuary.com/recipes/mediterranean-tomato-tart/"},
-    } ;;
->>>>>>> 69a64c80390a6ae892d87f941f75e6e565a05989
 
 }
