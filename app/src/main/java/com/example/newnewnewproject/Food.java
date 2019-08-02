69a64c80390a6ae892d87f941f75e6e565a05989
Jasmin Aquines <jasminaquines@gmail.com>
2019-08-01 21:21:33 -0500

    change
    fixed

--- app/src/main/java/com/example/newnewnewproject/Food.java
+++ app/src/main/java/com/example/newnewnewproject/Food.java
@@ -2,19 +2,19 @@ package com.example.newnewnewproject;
 
 public class Food {
     //to get food -> Food.french[number-1] = {"name","url"}
-    //Food.frech[number-1][0]
+    //Food.french[number-1][0]
     public static String[][] french = {{"Ratatouille Recipe", "https://www.thespruceeats.com/french-vegetarian-recipes-1375858"},
             {"Cauliflower Gratin Recipe ", "//www.thespruceeats.com/french-vegetarian-recipes-1375858"},
             {"Tempeh and Mushroom Bourguignon Recipe  ", "https://www.onegreenplanet.org/vegan-recipe/tempeh-and-mushroom-bourguignon/"},
             {"Potato Leek Quiche", "ttps://www.onegreenplanet.org/vegan-recipe/potato-leek-quiche/"},
             {"Sun - Dried Tomato and Zucchini Tarts", "https://www.onegreenplanet.org/vegan-recipe/sun-dried-tomato-and-zucchini-tarts/"},
-            {"Tomatoes Provencal ","https://www.onegreenplanet.org/vegan-recipe/tomatoes-provencal/"},
-            {"Vegan Leek Quiche ","https://holycowvegan.net/vegan-leek-quiche/"},
-            {"Vegan Savory Herb French Toast ","https://holycowvegan.net/vegan-savory-herb-french-toast/"},
-            {"A tres vegan cassoulet ","https://holycowvegan.net/vegan-cassoulet"},
-            {"Socca ( Farinata) - Chickpea Flour","https://simple-veganista.com/socca-farinata/"},
-            {"Veggie Pan Bagnat( White Bean Nicoise Salad Sandwich ","https://simple-veganista.com/pan-bagnat-white-bean-nicoise-salad/"},
-            {"Medterranean Tomato Tart ","https://veganuary.com/recipes/mediterranean-tomato-tart/"},
-
+            {"Tomatoes Provencal ", "https://www.onegreenplanet.org/vegan-recipe/tomatoes-provencal/"},
+            {"Vegan Leek Quiche ", "https://holycowvegan.net/vegan-leek-quiche/"},
+            {"Vegan Savory Herb French Toast ", "https://holycowvegan.net/vegan-savory-herb-french-toast/"},
+            {"A tres vegan cassoulet ", "https://holycowvegan.net/vegan-cassoulet"},
+            {"Socca ( Farinata ) - Chickpea Flour", "https://simple-veganista.com/socca-farinata/"},
+            {"Veggie Pan Bagnat( White Bean Nicoise Salad Sandwich ", "https://simple-veganista.com/pan-bagnat-white-bean-nicoise-salad/"},
+            {"Mediterranean Tomato Tart ", "https://veganuary.com/recipes/mediterranean-tomato-tart/"},
+    } ;;
 
 }
